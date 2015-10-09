22d813fff22686782ed92f6207981d7783dba100
Richard van der Dys <richard.vanderdys@gmail.com>
2015-10-09 12:44:52 -0500

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 3000);
+app.listen(port, function () {
+  console.log('Started server @ http://localhost:' + port);
+});
